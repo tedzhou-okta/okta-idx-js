4b82652f024d046fa77a06b23f1292c16a53914e
James Bui <james.bui@okta.com>
2020-05-07 18:00:21 -0700

    Update publish script to push to npm-topic

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,10 +5,11 @@
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
 export TEST_SUITE_TYPE="build"
-export PATH="${PATH}:$(yarn global bin)"
+export REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-topic"
 
-yarn global add @okta/ci-update-package
-yarn global add @okta/ci-pkginfo
+# Install required dependencies
+export PATH="${PATH}:$(yarn global bin)"
+yarn global add @okta/ci-append-sha
 
 if [ -n "${action_branch}" ]; then
   echo "Publishing from bacon task using branch ${action_branch}"
@@ -18,19 +19,16 @@ else
   TARGET_BRANCH=${BRANCH}
 fi
 
-if ! ci-update-package --branch ${TARGET_BRANCH}; then
+if ! ci-append-sha; then
   echo "ci-update-package failed! Exiting..."
   exit $FAILED_SETUP
 fi
 
+# Update default regsitry before publishing
+npm config set @okta:registry ${REGISTRY}
+
 if ! npm publish --unsafe-perm; then
   echo "npm publish failed! Exiting..."
-  exit ${PUBLISH_ARTIFACTORY_FAILURE}
-fi
-
-DATALOAD=$(ci-pkginfo -t dataload)
-if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
-  echo "artifactory_curl failed! Exiting..."
   exit ${PUBLISH_ARTIFACTORY_FAILURE}
 fi
 
