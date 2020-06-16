00649f5c94511617caa07223033b4fd303ae0b40
Brett Ritter <swiftone@swiftone.org>
2020-06-16 09:58:01 -0700

    clean up parsers v1 exports

--- src/v1/parsers.js
+++ src/v1/parsers.js
@@ -1,10 +1,5 @@
-import generateIdxAction from './generateIdxAction';
 import makeIdxState from './makeIdxState';
-import { parseIdxResponse } from './idxResponseParser';
-// import remediationParser from './remediationParser';
 
 export default {
-  generateIdxAction,
-  parseIdxResponse,
   makeIdxState,
 };
